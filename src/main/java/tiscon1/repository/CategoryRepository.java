578d5ddc44768ad7a90db035fd68d16ed2f9664d
亮太 <kbc13a15@stu.kawahara.ac.jp>
2016-02-21 10:28:53 +0900

    トップページの負荷の検証

--- src/main/java/tiscon1/repository/CategoryRepository.java
+++ src/main/java/tiscon1/repository/CategoryRepository.java
@@ -11,5 +11,23 @@ import java.util.List;
 public interface CategoryRepository {
     List<Item> findTop10(String genreId, String subgenreId) throws IOException;
 
+    /**
+     * よっしー加筆
+     * トップページの負荷対策
+     * 取得するアイテム数を3個にしたメソッド
+     * @param genreId
+     * @param subgenreId
+     * @return
+     * @throws IOException
+     */
+    List<Item> findTop3(String genreId, String subgenreId) throws IOException;
+
+    /**
+     * よっしー加筆
+     * テスト用の空のリストを返すメソッド
+     * @return
+     */
+    List<Item> findEmptyList();
+
     Item searchItem(String genreId, String id) throws IOException;
 }
